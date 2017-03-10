c4bc686a5409d4d095656307a88c42a37608f4dc
Kane Balagtey <kbalagtey@tacitknowledge.com>
2017-03-10 18:02:54 +0000

    update mock properties file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import './ngs-order-placement/grails-app/assets/javascripts/app/checkout/checkoutRoot';
+// import './ngs-order-placement/grails-app/assets/javascripts/app/checkout/checkoutRoot';
+import './ngs-order-placement/grails-app/assets/javascripts/app/styleguide/checkoutComponents/checkoutComponents';
 import './index.scss';
 
